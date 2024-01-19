4d83ffe59445e3e753b2cb6ae3c68b5007f91f20
Kaique Yamamoto | Full Stack Developer <kaique@redblock.com.br>
2024-01-19 14:43:04 -0300

    fix: add resource login

---
+++ src/login/login.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { CreateLoginDto } from './dto/create-login.dto';
+import { UpdateLoginDto } from './dto/update-login.dto';
+import KcAdminClient from '@keycloak/keycloak-admin-client';
+
+@Injectable()
+export class LoginService {
+  private adminClientKc = new KcAdminClient({
+    baseUrl: 
+  })
+
+  create(createLoginDto: CreateLoginDto) {
+    return 'This action adds a new login';
+  }
+
+  findAll() {
+    return `This action returns all login`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} login`;
+  }
+
+  update(id: number, updateLoginDto: UpdateLoginDto) {
+    return `This action updates a #${id} login`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} login`;
+  }
+}
